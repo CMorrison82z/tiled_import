bcf5c8844c0c2dbf9bf1b29351c7c907cab24bf6
chris <cmorrison82z@proton.me>
2025-01-20 00:56:34 +0000

    change todo to eprintln for now

--- crates/bevy_tiled_loader/src/load.rs
+++ crates/bevy_tiled_loader/src/load.rs
@@ -234,7 +234,7 @@ fn load_tmx(load_context: &mut LoadContext, tm: TiledMap) -> Result<TiledMapAsse
                     }
                     LayerType::Group => println!("Group layer {name}"),
                     _ => {
-                        todo!("Layer {content:#?} is not currently handled.");
+                        eprintln!("Layer {content:#?} is not currently handled.");
                     }
                 }
             });
