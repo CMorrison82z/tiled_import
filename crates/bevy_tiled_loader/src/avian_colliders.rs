b119af4720b3caffbd6c46a7de8161ad0d8bd78e
chris <cmorrison82z@proton.me>
2025-09-23 01:42:15 +0100

    bits

--- crates/bevy_tiled_loader/src/avian_colliders.rs
+++ crates/bevy_tiled_loader/src/avian_colliders.rs
@@ -157,10 +157,10 @@ pub fn shapes_to_collider(s: Shapes<FloatPoint<f32>>) -> impl Bundle {
             (
                 SceneSerializedComponents::SerCollider,
                 bincode::serialize(&Collider::trimesh_with_config(points, triangles, TrimeshFlags::from_bits(
-                    TrimeshFlags::FIX_INTERNAL_EDGES |
+                    (TrimeshFlags::FIX_INTERNAL_EDGES |
                     TrimeshFlags::DELETE_DEGENERATE_TRIANGLES |
-                    TrimeshFlags::DELETE_DUPLICATE_TRIANGLES
-                ).unwrap()).unwrap())
+                    TrimeshFlags::DELETE_DUPLICATE_TRIANGLES).bits()
+                ).unwrap())).unwrap()
                 // bincode::serialize(&Collider::compound(triangles)).unwrap(),
                 // bincode::serialize(&Collider::convex_decomposition(points, edges)).unwrap(),
             ),
